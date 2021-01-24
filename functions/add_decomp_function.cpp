c88c24b574e80159dd1d429440ddeb5541558041
Sohyeon Kim <ksh2904@g.skku.edu>
2021-01-24 22:03:33 +0900

    update add_decomp function using cpp

--- functions/add_decomp_function.cpp
+++ functions/add_decomp_function.cpp
@@ -4,154 +4,184 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
 
-//Sum_Mat.cpp
 //[[Rcpp::export]]
-NumericMatrix Sum_Mat(List V){
+List add_decomp(double delta, double lambda_1, double lambda_2, double tol_error, int max_iter, arma::mat X,
+                arma::mat Y, List V, arma::mat Phi, arma::mat theta_0, arma::mat alpha_0, NumericVector tau_seq) {
   Function f("Reduce");
-  return f(Named("f") = "+", Named("x") = V);
-}
-
-//prod_AB.cpp
-//[[Rcpp::export]]
-arma::mat prod_AB(arma::mat A, arma::mat B, bool A_t, bool B_t) {
-  if(A_t == true) {
-    if(B_t == true) return A.t()*B.t();
-    if(B_t == false) return A.t()*B;
-  }
-  if(A_t == false) {
-    if(B_t == true) return A*B.t();
-    if(B_t == false) return A*B;
-  }
-}
-
-//update_eta.cpp
-//[[Rcpp::export]]
-arma::mat update_eta(double delta, arma::mat sum_VV, arma::mat W, arma::mat Theta, 
-                     arma::mat sum_VU, arma::mat sum_V, arma::mat Y, arma::mat Z, arma::mat sum_VE) {
-  double dim = sum_VV.n_rows;
-  arma::mat diag(dim, dim, arma::fill::eye);
-  arma::mat First = sum_VV + diag;
-  arma::mat Second = W + delta*Theta + sum_VU + delta*sum_V.t()*(Y - Z) - delta * sum_VE;
-  arma::mat output = inv(First) * Second;
-  return output*(1/delta);
-}
-
-//update_theta.cpp
-//[[Rcpp::export]]
-arma::mat update_theta(double delta, double lambda_2, double K, arma::mat Eta, arma::mat W) {
-  double n = W.n_rows;
-  double m = W.n_cols;
-  double p = n/K - 1;
-  arma::mat Theta(n, m);
+  //delta = step size
+  //lambda_1 = low rank penalty
+  //lambda_2 = sparse penalty
   
-  for(int g=0; g<m; g++) {
-    arma::vec r_j = Eta.col(g) - (W.col(g)/delta);
-    for(int j=0; j<(p+1); j++) {
-      arma::vec sub_vec = r_j.subvec(K*j, K*(j+1)-1);
-      double accum = 0;
-      for(int i =0; i<K; i++) {
-        accum += sub_vec[i]*sub_vec[i];
-      }
-      double norm = sqrt(accum);
-      double value = 1 - (lambda_2/(delta*norm));
-      if(value >= 0) {
-        Theta.submat(K*j, g, K*(j+1)-1, g) = value * sub_vec;
-      } else {
-        Theta.submat(K*j, g, K*(j+1)-1, g) = arma::vec(K);
-      }
-    }
-  }
-  return Theta;
-}
-
-//update_alpha.cpp
-//[[Rcpp::export]]
-List update_alpha(double delta, double lambda_1, arma::mat Y, arma::mat X, arma::mat H, 
-                  List V, List E, List U) {
-  Function f("Reduce");
-  double b = V.size();
-  arma::mat Sum_V = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = V)));
-  arma::mat Sum_E = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = E)));
-  arma::mat Sum_U = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = U)));
-  arma::mat obj = Y - (1/b)*Sum_V*H - (1/b)*Sum_E - (1/(delta*b))*Sum_U;
-  double n = obj.n_rows;
-  double p = obj.n_cols;
-  arma::mat P;
-  arma::mat Q;
-  arma::vec d;
-  arma::svd(P, d, Q, obj);
-  if(p > n) {Q = Q.cols(0, n-1);}
-  if(n > p) {P = P.cols(0, p-1);}
-  arma::vec d_new = d - lambda_1/(delta*b);
-  NumericVector diag_entry = ifelse(as<NumericVector>(wrap(d_new)) > 0, as<NumericVector>(wrap(d_new)), 0);
-  arma::mat D_new = diagmat(as<arma::vec>(wrap(diag_entry)));
-  arma::mat Z_new = P*D_new*Q.t();
-  arma::mat alpha_new = inv(X.t()*X)*X.t()*Z_new;
-  return List::create(Named("Z_new") = Z_new, Named("alpha_new") = alpha_new);
-}
-
-//update_e.cpp
-//[[Rcpp::export]]
-List update_e(double delta, List U, List V, arma::mat Y, arma::mat X, arma::mat A, arma::mat H, 
-              NumericVector tau_seq) {
-  double n = Y.n_rows;
-  double b = U.size();
+  //initial value
+  double n = X.n_rows;
+  double p = X.n_cols - 1;
+  double K = Phi.n_cols;
+  double b = Phi.n_rows;
   double m = Y.n_cols;
-  arma::mat Z = X*A;
-  List e_new = List(b);
-  for(int l = 0; l < b; l++) {
-    arma::mat mat_E = arma::mat(n, m);
-    arma::mat VH = as<arma::mat>(wrap(V[l]))*H;
-    arma::mat Error = Y - Z - VH;
-    arma::mat mat_U = U[l];
-    for(int g = 0; g < m; g++) {
-      for(int i = 0; i < n; i++) {
-        if(Error(i, g) + mat_U(i, g)/delta < (tau_seq(l)-1)/(n*delta)) {
-          mat_E(i, g) = Error(i, g) + mat_U(i, g)/delta - (tau_seq(l)-1)/(n*delta);
-        } else if(Error(i, g) + mat_U(i, g)/delta > tau_seq(l)/(n*delta)) {
-          mat_E(i, g) = Error(i, g) + mat_U(i, g)/delta - tau_seq(l)/(n*delta);
-        } else {mat_E(i, g) = 0;}
+  arma::mat eta_old = theta_0;
+  arma::mat theta_old = theta_0;
+  arma::mat alpha_old = alpha_0;
+  arma::mat Z_old = X * alpha_old;
+  List e_old(b);
+  for(int l =0; l<b; l++) {
+    arma::mat e_old_mat = Y  - Z_old - as<arma::mat>(wrap(V[l])) *eta_old;
+    e_old[l] = e_old_mat;
+  }
+  List u_old(b);
+  for(int l=0; l<b; l++) {
+    arma::mat u_old_mat(n, m);
+    u_old_mat.zeros(n, m);
+    u_old[l] = u_old_mat;
+  }
+  arma::mat w_old;
+  w_old.zeros((p+1)*K, m);
+  arma::mat iter_error(max_iter, 6);
+  arma::mat sum_V = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = V)));
+  List VV_prod(b);
+  for(int l=0; l<b; l++) {
+    arma::mat V_mat = V[l];
+    VV_prod[l] = V_mat.t() * V_mat;
+  }
+  arma::mat sum_VV = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = VV_prod)));
+  
+  //iteration
+  for(int iter=0; iter<max_iter; iter++) {
+    //Process for eta
+    List Vu_prod(b);
+    List Ve_prod(b);
+    for(int l=0; l<b; l++) {
+      arma::mat V_mat = V[l];
+      arma::mat u_old_mat = u_old[l];
+      arma::mat e_old_mat = e_old[l];
+      Vu_prod[l] = V_mat.t() * u_old_mat;
+      Ve_prod[l] = V_mat.t() * e_old_mat;
+    }
+    arma::mat sum_Vu = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = Vu_prod)));
+    arma::mat sum_Ve = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = Ve_prod)));
+    double dim = sum_VV.n_rows;
+    arma::mat diag(dim, dim, arma::fill::eye);
+    arma::mat First = sum_VV + diag;
+    arma::mat Second = w_old + delta * theta_old + sum_Vu + delta*sum_V.t()*(Y-Z_old) - delta*sum_Ve;
+    arma::mat output = inv(First) * Second;
+    arma::mat eta_new = output*(1/delta);
+    
+    //Process for theta
+    arma::mat theta_new((p+1)*K, m);
+    for(int g = 0; g<m; g++) {
+      arma::vec r_j = eta_new.col(g) - (w_old.col(g)/delta);
+      for(int j=0; j<(p+1); j++) {
+        arma::vec sub_vec = r_j.subvec(K*j, K*(j+1)-1);
+        double accum = 0;
+        for(int i=0; i<K; i++) {
+          accum += sub_vec[i]*sub_vec[i];
+        }
+        double norm = sqrt(accum);
+        double value = 1 - (lambda_2/(delta*norm));
+        if(value >= 0) {
+          theta_new.submat(K*j, g, K*(j+1)-1, g) = value * sub_vec;
+        } else {
+          theta_new.submat(K*j, g, K*(j+1)-1, g) = arma::vec(K);
+        }
       }
     }
-    e_new[l] = mat_E;
-  }
-  return e_new;
-}
-
-//update_multipliers.cpp
-//[[Rcpp::export]]
-List update_multi(List U_old, arma::mat W_old, double delta, arma::mat Y, arma::mat X, arma::mat A, arma::mat H,
-                  arma::mat Theta, List V, List E, bool is_u) {
-  if(is_u == true) {
-    double b = U_old.size();
-    List U_new = List(b);
+    
+    //Process for Z = XA
+    arma::mat sum_E = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = e_old)));
+    arma::mat sum_U = as<arma::mat>(wrap(f(Named("f") = "+", Named("x") = u_old)));
+    arma::mat obj = Y - (1/b)*sum_V*eta_new - (1/b)*sum_E - (1/(delta*b))*sum_U;
+    arma::mat P;
+    arma::mat Q;
+    arma::vec d;
+    arma::svd(P, d, Q, obj);
+    if(m > n) {Q = Q.cols(0, n-1);}
+    if(n > m) {P = P.cols(0, m-1);}
+    arma::vec d_new = d - lambda_1/(delta*b);
+    NumericVector diag_entry = ifelse(as<NumericVector>(wrap(d_new)) > 0, 
+                                      as<NumericVector>(wrap(d_new)), 0);
+    arma::mat D_new = diagmat(as<arma::vec>(wrap(diag_entry)));
+    arma::mat Z_new = P*D_new*Q.t();
+    arma::mat alpha_new = inv(X.t()*X)*X.t()*Z_new;
+    
+    //Process for e
+    List e_new = List(b);
     for(int l = 0; l < b; l++) {
-      arma::mat VH = as<arma::mat>(wrap(V[l]))*H;
-      arma::mat mat_E = E[l];
-      arma::mat mat_U_old = U_old[l];
-      arma::mat mat_U = mat_U_old + delta * (Y - X*A - VH - mat_E);
-      U_new[l] = mat_U;
+      arma::mat mat_E = arma::mat(n, m);
+      arma::mat VH = as<arma::mat>(wrap(V[l]))*eta_new;
+      arma::mat Error = Y - Z_new - VH;
+      arma::mat mat_U = u_old[l];
+      for(int g=0; g<m; g++) {
+        for(int i=0; i<n; i++) {
+          if(Error(i, g) + mat_U(i, g)/delta < (tau_seq(l)-1)/(n*delta)) {
+            mat_E(i, g) = Error(i, g) + mat_U(i, g)/delta - (tau_seq(l)-1)/(n*delta);
+          } else if(Error(i, g) + mat_U(i, g)/delta > tau_seq(l)/(n*delta)) {
+            mat_E(i, g) = Error(i, g) + mat_U(i, g)/delta - tau_seq(l)/(n*delta);
+          } else {mat_E(i, g) = 0;}
+        }
+      }
+      e_new[l] = mat_E;
     }
-    return U_new;
-  } else {
-    arma::mat W_new = W_old + delta *(Theta - H);
-    return List::create(W_new);
+    
+    //Process for multiplier u
+    List u_new = List(b);
+    for(int l=0; l<b; l++) {
+      arma::mat VH = as<arma::mat>(wrap(V[l]))*eta_new;
+      arma::mat mat_E = e_new[l];
+      arma::mat mat_U_old = u_old[l];
+      arma::mat mat_U = mat_U_old + delta * (Y - Z_new - VH - mat_E);
+      u_new[l] = mat_U;
+    }
+    
+    //Process for multiplier w
+    arma::mat w_new = w_old + delta * (theta_new - eta_new);
+    
+    //Update iteration error
+    iter_error(iter, 0) = norm(eta_old - eta_new, "fro"); 
+    iter_error(iter, 1) = norm(theta_old - theta_new, "fro");
+    iter_error(iter, 2) = norm(alpha_old - alpha_new, "fro");
+    List diff_e = List(b);
+    List diff_u = List(b);
+    for(int l=0; l<b; l++) {
+      double diff_e_norm = norm(as<arma::mat>(wrap(e_old[l])) - as<arma::mat>(wrap(e_new[l])), "fro");
+      diff_e[l] = diff_e_norm;
+      double diff_u_norm = norm(as<arma::mat>(wrap(u_old[l])) - as<arma::mat>(wrap(u_new[l])), "fro");
+      diff_u[l] = diff_u_norm;
+    }
+    iter_error(iter, 3) = as<double>(wrap(f(Named("f") = "+", Named("x") = diff_e)));
+    iter_error(iter, 4) = as<double>(wrap(f(Named("f") = "+", Named("x") = diff_u)));
+    iter_error(iter, 5) = norm(w_old - w_new, "fro");
+    
+    //End the loop
+    arma::vec threshold = as<arma::vec>(wrap(iter_error.row(iter)));
+    double count = 0;
+    for(int i=0; i<6; i++) {
+      count += threshold[i];
+    }
+    if(count < tol_error) break;
+    
+    //replace old parameter
+    eta_old = eta_new;
+    theta_old = theta_new;
+    Z_old = Z_new;
+    alpha_old = alpha_new;
+    e_old = e_new;
+    u_old = u_new;
+    w_old = w_new;
+    
   }
-}
-
-//calculate_iteration_error.cpp
-//[[Rcpp::export]]
-double calc_err(List Old, List New) {
-  Function f("Reduce");
-  double b = Old.size();
-  List FR_list = List(b);
-  for(int l = 0; l < b; l++) {
-    arma::mat Old_mat = Old[l];
-    arma::mat New_mat = New[l];
-    FR_list[l] = norm(Old_mat - New_mat, "fro");
-  }
-  double FR_sum = as<double>(wrap(f(Named("f") = "+", Named("x") = FR_list)));
-  return FR_sum;
+  //result
+  List estimate = List::create(Named("eta") = eta_old, 
+                               Named("theta") = theta_old, 
+                               Named("alpha") = alpha_old, 
+                               Named("e") = e_old, 
+                               Named("u") = u_old, 
+                               Named("w") = w_old, 
+                               Named("iter_error") = iter_error);
+  
+  
+  
+  
+  
+  return estimate;
 }
 
 //calculate_kronecker_product_V.cpp
