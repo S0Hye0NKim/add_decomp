df758b1759fe585942744f35018f02ae16326afa
Sohyeon Kim <ksh2904@g.skku.edu>
2021-05-10 14:51:23 +0900

    change weight to SCAD derivative function

--- functions/add_decomp_function.R
+++ functions/add_decomp_function.R
@@ -1,3 +1,9 @@
+scad_deriv <- function(x, lambda, gamma = 3.7) {
+  output <- ifelse(abs(x) <= lambda, lambda, 
+                   ifelse(abs(x) >= gamma*lambda, 0, (gamma*lambda - abs(x))/(gamma - 1)))
+  return(output)
+}
+
 add_decomp_r <- function(delta, lambda_1, lambda_2, tol_error, max_iter, X, Y, V, Phi, 
                          theta_0, Z_0, tau_seq, weight = TRUE) {
   # delta = step size
@@ -41,12 +47,13 @@ add_decomp_r <- function(delta, lambda_1, lambda_2, tol_error, max_iter, X, Y, V
     for (g in 1:m) {
       for(j in 1:(p+1)) {
         theta_tilde <- theta_0[(K*(j-1) +1):(j*K), g]
-        norm_theta_tilde <- ifelse(weight == TRUE, (theta_tilde^2) %>% sum %>% sqrt, 1)  # weight = 1/norm_theta_tilde
+        weight_theta <- ifelse(weight == TRUE, (theta_tilde^2) %>% sum %>% sqrt 
+                               %>% scad_deriv(., lambda = lambda_2), 1)  # weight = scad_deriv(theta_norm)
         eta_j_g <- eta_new[(K*(j-1) +1):(j*K), g]
         w_j_g <- w_old[(K*(j-1) +1):(j*K), g]
         r_j_g <- eta_j_g - (w_j_g/delta)
         norm_r_j_g <- (r_j_g^2) %>% sum %>% sqrt
-        value <- 1 - (lambda_2/(delta *norm_r_j_g*norm_theta_tilde))
+        value <- 1 - (lambda_2/(delta *norm_r_j_g)) * weight_theta
         if(value >= 0) {
           theta_new[(K*(j-1) +1):(j*K), g] <- value * r_j_g
         } else {theta_new[(K*(j-1) +1):(j*K), g] <- 0}
@@ -62,8 +69,9 @@ add_decomp_r <- function(delta, lambda_1, lambda_2, tol_error, max_iter, X, Y, V
     SVD <- svd(obj)
     if(weight == TRUE) {
       sing_val_Z_0 <- svd(Z_0) %>% .$d
-    } else {sing_val_Z_0 <- rep(1, length(svd(Z_0) %>% .$d))}  # weight = 1/sing_val_Z_0
-    new_singular <- sapply(SVD$d - lambda_1/(delta*b*sing_val_Z_0), FUN = function(x) max(x, 0))
+      weight_Z <- scad_deriv(sing_val_Z_0, lambda = lambda_1)
+    } else {weight_Z <- rep(1, length(svd(Z_0) %>% .$d))}  # weight = 1/sing_val_Z_0
+    new_singular <- sapply(SVD$d - (lambda_1*weight_Z)/(delta*b), FUN = function(x) max(x, 0))
     Z_new <- SVD$u %*% diag(new_singular) %*% t(SVD$v)
     
     # Process for e
